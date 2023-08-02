149f3dc45c03185b8e422c45699697dc2f968bba
Stephen Hullender <shullender0907@gmail.com>
2023-08-02 15:30:32 -0700

    complete portfolio sample with working modal and selection

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -9,9 +9,13 @@ import colors from '../../constants/colors';
 const PortfolioPage = () => {
   const [showDescriptionModal, setShowDescriptionModal] = useState(false);
   const [hoverStates, setHoverStates] = useState([]);
-  const [selectImage, setSelectImage] = useState({
-    title: "", desc: "", date: new Date()
-  });
+
+  const initSelectImage = { link: "", title: "", desc: "", date: new Date() };
+  const [selectImage, setSelectImage] = useState(initSelectImage);
+
+  const PORTFOLIO_SECTION_WIDTH = '720px';
+  const DISPLAY_IMAGE_ON_MODAL = '100px';
+  const MODAL_MOVE_UP = '-77.5em';
 
   useEffect(() => {
     const initHover = Array(PORTFOLIO_TEXT.length).fill(false);
@@ -19,20 +23,106 @@ const PortfolioPage = () => {
   }, []);
 
   const MediaDescriptionModal = () => {
+    const { link, title, desc, date } = selectImage;
     return (
-      <>
-        <span>{ selectImage.title }</span>
-        <span>{ selectImage.desc }</span>
-        <small>{ formatDate(selectImage.date) ?? "Date unknown" }</small>
-      </>
+      <FlexRow edits={{
+        position: 'fixed',
+        zIndex: 995,
+        width: '600px',
+        height: 'fit-content',
+        maxHeight: '80px',
+        padding: '1em', 
+        backgroundColor: colors.snowWhite,
+        border: `5px solid ${colors.herbalGreen}`,
+        marginTop: MODAL_MOVE_UP,
+        left: '50%',
+        transform: 'translateX(-50%)'
+      }}>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          paddingRight: '1em',
+          left: 0
+        }}>
+          <FlexColumn>
+          <div style={{
+            width: DISPLAY_IMAGE_ON_MODAL * 1.125,
+            display: 'flex',
+            justifyContent: 'center',
+            height: '5px',
+            marginBottom: '0.25em',
+            backgroundColor: colors.herbalGreen
+          }}/>
+          <img 
+            src={link}
+            style={{
+              width: DISPLAY_IMAGE_ON_MODAL,
+              height: 'fit-content',
+              border: `2px solid ${colors.herbalGreen}`
+            }}
+          />
+          <div style={{
+            width: DISPLAY_IMAGE_ON_MODAL * 1.125,
+            display: 'flex',
+            justifyContent: 'center',
+            height: '5px',
+            marginTop: '0.25em',
+            backgroundColor: colors.herbalGreen
+          }}/>
+          </FlexColumn>
+        </div>
+        <FlexColumn edits={{
+          display: 'flex',
+          justifyContent: 'center'
+        }}>
+          <FlexRow edits={{
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
+            <span style={{
+              fontWeight: 'bold',
+              fontSize: '17px'
+            }}>
+              { title }
+            </span>
+            <small>{ formatDate(date) ?? "Date unknown" }</small>
+          </FlexRow>
+          <span>{ desc }</span>
+          <FlexRow edits={{
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
+            <small>
+              <a href={link} target="_blank">
+                See full picture in new tab
+              </a>
+            </small>
+            <small 
+              onClick={() => {
+                setShowDescriptionModal(false);
+                setSelectImage(initSelectImage);
+              }}
+              style={{
+                fontWeight: 'bold',
+                color: colors.brightOrange,
+                textDecoration: 'underline',
+                cursor: 'pointer'
+              }}  
+            >
+                Close window
+            </small>
+          </FlexRow>
+        </FlexColumn>
+      </FlexRow>
     )
   }
 
-  const handleImageClick = (title, desc, date) => {
+  const handleImageClick = (media) => {
+    const { link, title, desc, date } = media;
     if (!showDescriptionModal) {
       setShowDescriptionModal(true);
     }
-    setSelectImage({ title, desc, date });
+    setSelectImage({ link, title, desc, date });
   }
 
   const handleImageHover = (i, isHovered) => {
@@ -44,47 +134,136 @@ const PortfolioPage = () => {
   return (
     <>
       <h2>Portfolio</h2>
+      <p>
+        Click on each picture for a brief explanation of what the body of work means.
+      </p>
       <FlexColumn>
         {
           PORTFOLIO_TEXT.map((element, index) => {
             const { title, desc, dates, media } = element;
+
+            const half = media.length/2;
+            const firstColumn = media.slice(0, half), secondColumn = media.slice(half);
+            
+            const IMAGE_WIDTH = '333px';
+            const MAX_HEIGHT = IMAGE_WIDTH * 0.85;
+
             return (
-              <FlexColumn key={index}>
-                <FlexRow>
-                  <span>{title}</span>
-                  <small>
-                    {`${formatDate(dates.start)} - ${formatDate(dates.end) ?? "Present"}`}
-                  </small>
+              <FlexColumn key={index} edits={{
+                width: PORTFOLIO_SECTION_WIDTH
+              }}>
+                <div style={{ 
+                  border: `5px solid ${colors.seaBlue}`,
+                  padding: '1em'
+                }}>
+                  <FlexRow edits={{
+                    display: 'flex',
+                    justifyContent: 'space-between'
+                  }}>
+                    <span style={{
+                      fontWeight: 'bold',
+                      fontSize: '20px',
+                      color: colors.brightBlue
+                    }}>
+                      {title}
+                    </span>
+                    <small style={{ 
+                      color: colors.jetBlack,
+                      paddingTop: '0.333em'
+                    }}>
+                      {`${formatDate(dates.start)} - ${formatDate(dates.end) ?? "Present"}`}
+                    </small>
+                  </FlexRow>
+                  <blockquote style={{
+                    fontSize: '18px',
+                    color: colors.brightBlue
+                  }}>
+                    { desc }
+                  </blockquote>
+                </div>
+                <div style={{
+                  backgroundColor: colors.seaBlue,
+                  width: 'fit-content',
+                  height: 'fit-content'
+                }}>
+                <FlexRow edits={{
+                  width: PORTFOLIO_SECTION_WIDTH,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  padding: '0.25em 0'
+                }}>
+                    <FlexColumn>
+                      {
+                        firstColumn.map((m, i) => {
+                          return (
+                            <div style={{
+                              display: 'flex',
+                              alignItems: 'center'
+                            }}>
+                              <img 
+                                key={i}
+                                style={{ 
+                                  border: `1px solid ${colors.brightBlue}`,
+                                  cursor: hoverStates[i] ? 'pointer' : 'default',
+                                  opacity: hoverStates[i] ? 0.9 : 1.0,
+                                  width: IMAGE_WIDTH,
+                                  height: 'fit-content',
+                                  maxHeight: MAX_HEIGHT,
+                                  margin: '0.5em'
+                                }}
+                                onMouseEnter={() => handleImageHover(i, true)}
+                                onMouseLeave={() => handleImageHover(i, false)}
+                                onClick={() => handleImageClick(m)}
+                                src={m.link}
+                              />
+                            </div>
+                          )
+                        })
+                      }
+                    </FlexColumn>  
+                    <FlexColumn>
+                      {
+                        secondColumn.map((m, i) => {
+                          i += half;
+                          return (
+                            <div style={{
+                              display: 'flex',
+                              alignItems: 'center'
+                            }}>
+                              <img 
+                                key={i}
+                                style={{ 
+                                  border: `1px solid ${colors.brightBlue}`,
+                                  cursor: hoverStates[i] ? 'pointer' : 'default',
+                                  opacity: hoverStates[i] ? 0.9 : 1.0,
+                                  width: IMAGE_WIDTH,
+                                  height: 'fit-content',
+                                  maxHeight: MAX_HEIGHT,
+                                  margin: '0.5em'
+                                }}
+                                onMouseEnter={() => handleImageHover(i, true)}
+                                onMouseLeave={() => handleImageHover(i, false)}
+                                onClick={() => handleImageClick(m)}
+                                src={m.link}
+                              />
+                            </div>
+                          )
+                        })
+                      }
+                    </FlexColumn>
                 </FlexRow>
-                <span>{desc}</span>
-                <div>
-                  {
-                    media.map((m, i) => {
-                      const { link, title, desc, date } = m;
-                      return (
-                        <img 
-                          key={i}
-                          style={{ 
-                            border: `1px solid ${colors.brightBlue}`,
-                            cursor: hoverStates[i] ? 'pointer' : 'default',
-                            opacity: hoverStates[i] ? 0.9 : 1.0,
-                            height: '100px',
-                            width: 'fit-content'
-                          }}
-                          onMouseEnter={() => handleImageHover(i, true)}
-                          onMouseLeave={() => handleImageHover(i, false)}
-                          onClick={() => handleImageClick(title, desc, date)}
-                          src={link}
-                        />
-                      )
-                    })
-                  }
                 </div>
               </FlexColumn>
             )
           })
         }
       </FlexColumn>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      { /* :-) don't mind these breaks :-) */ }
       {
         selectImage.title != "" &&
         <MediaDescriptionModal />
